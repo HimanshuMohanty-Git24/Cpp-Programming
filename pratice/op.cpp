648f5939b084f7e415f3f54e1b31e85047563837
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-02-28 00:22:06 +0530

    change in op.ccp file

--- pratice/op.cpp
+++ pratice/op.cpp
@@ -7,6 +7,7 @@ int main(int argc, char const *argv[])
 {
     float num1,num2,add,sub,multiply;
     float divide;
+    int module;
 
     cout<<"\nEnter 2 number(First number>Second Number): ";
     cin>>num1>>num2;
@@ -14,9 +15,11 @@ int main(int argc, char const *argv[])
     sub = num1-num2;
     multiply = num1*num2;
     divide = num1/num2;
+    module = (int)num1%(int)num2;
     cout<<"Addition of 2 number:"<<add
     <<"\nSubtraction of 2 number:"<<sub
     <<"\nMultiplication of 2 number:"<<multiply
-    <<setprecision(2)<<"\nDivision of 2 number:"<<divide;
+    <<setprecision(2)<<"\nDivision of 2 number:"<<divide
+    <<"\nModulus of 2 numbers:"<<module;
     return 0;
 }
