58634104f29d90063a9c1685e487e73d25366f21
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-06-30 10:33:51 +0530

    Size of data types

---
+++ AbdulBari cpp course/Size_ofDataType.cpp
@@ -0,0 +1,22 @@
+//Size of data types
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  short short_type;
+  int int_type;
+  float float_type;
+  char Char_type;
+  double doub_type;
+  long long_type;
+  cout<<"\tThese are the size of all the data types"<<endl;
+  cout<<"\t-----------------------------------------"<<endl;
+  cout<<endl<<"The size of Char Data Type is:"<<sizeof(Char_type)<<"Bytes"<<endl;
+  cout<<endl<<"The size of Short Data Type is:"<<sizeof(short_type)<<"Bytes"<<endl;
+  cout<<endl<<"The size of int Data Type is:"<<sizeof(int_type)<<"Bytes"<<endl;
+  cout<<endl<<"The size of float Data Type is:"<<sizeof(float_type)<<"Bytes"<<endl;
+  cout<<endl<<"The size of Double Data Type is:"<<sizeof(doub_type)<<"Bytes"<<endl;
+  cout<<endl<<"The size of Long Data Type is:"<<sizeof(long_type)<<"Bytes"<<endl;
+}
