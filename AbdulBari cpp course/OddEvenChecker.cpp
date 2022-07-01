617e3a18da4ca3e3bcfc63b83a13f6e52c44bc17
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-07-01 20:57:52 +0530

    OddEven

---
+++ AbdulBari cpp course/OddEvenChecker.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter a number:";
+    cin>>num;
+    if(num%2==0)
+        cout<<" "<<num<<" is a even number";
+    else
+        cout<<" "<<num<<" is a odd number";
+
+    return 0;
+}
