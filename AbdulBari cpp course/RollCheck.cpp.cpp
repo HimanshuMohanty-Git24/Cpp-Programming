885f9d0262772cab001fe90bd5d8615b50a9f593
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-07-01 20:59:54 +0530

    Roll Check

---
+++ AbdulBari cpp course/RollCheck.cpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int roll;
+    cout<<"Enter your Roll No."<<endl;
+    cin>>roll;
+    if(roll>0)
+    {
+        cout<<"Valid Roll No."<<endl;
+    }
+    else
+    {
+        cout<<"Invalid Roll No."<<endl;
+    }
+    return 0;
+}
