baf7b8d159b24d99e517abbcd80a41f90b5f050e
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-08-11 15:07:09 +0530

    Grading Student HackeRank Ques

--- CP/GradingStudent.cpp
+++ CP/GradingStudent.cpp
@@ -5,15 +5,14 @@ using namespace std;
 int main()
 {
     int n;
+    cin >> n;
     int grade[n];
     int mul[n];
-    cin >> n;
     for (int i = 0; i < n; i++)
     {
         cin >> grade[i];
         mul[i] = ((grade[i] / 5) * 5) + 5;
     }
-    cout << endl;
     for (int i = 0; i < n; i++)
     {
         if (grade[i] >= 38)
